08ee9c2466bbe95ed01699232d8e4b5359cea80f
Hector Corona <coronahe95@gmail.com>
2026-05-26 22:48:13 -0700

    some progress

--- Easy/PlusOne.cpp
+++ Easy/PlusOne.cpp
@@ -4,42 +4,46 @@
 class Solution {
 public:
     std::vector<int> plusOne(std::vector<int>& digits) {
-        std::vector<int> holder;
-        for(int i = 0; i < digits.size(); i++) {
-            holder[i] = digits[i];
+        std::vector<int> holder; // temp holder
+        for(int i = 0; i <= digits.size(); i++) {
+            if(i == digits.size()-1) {
+                if(digits[i] == 9) {
+                    //if 9 then current turns to 1 and then a 0 at then
+                    digits[i] = 1;
+                    digits.push_back(0);
+                    break;
+                } else {
+                    digits[i] += 1;
+                }
+            }
         }
+        std::cout << "[";
         for(int i = 0; i < digits.size(); i++) {
             std::cout << digits[i];
+            if(i != digits.size()-1) {
+                std::cout << ", ";
+            }
         }
-
-
-
-        for(int i = 0; i < digits.size(); i++) {
-            if(i == digits.size() - 1 && digits[i] <= 9) {
-                digits[i] + 1;
-            } else {
-
-            }        
-        }
-        //return std::vector<2>;
+        std::cout << "]";
         return digits;
     }
-};
+    
+};  
 
 int main() {
     Solution s;
     std::vector<int> user;
-    user.push_back(1);
-    user.push_back(2);
-    user.push_back(3);
-    user.push_back(4);
+    //user.push_back(1);
+    //user.push_back(2);
+    //user.push_back(3);
+    //user.push_back(4);
+    user.push_back(9);
     /*for(std::vector<int>::iterator it = user.begin(); it != user.end(); it++) {
         std::cout << *it << " ";
     }*/
     /*for(int value : user) {
         std::cout << value << " ";
     }*/
-    // visual representation of the vector
     std::cout << "[";
     for(int i = 0; i < user.size(); i++) {
         std::cout << user[i];
@@ -52,7 +56,8 @@ int main() {
     // passing vector into PlusOne
     std::vector<int> addedVec = s.plusOne(user);
     for(std::vector<int>::iterator it = addedVec.begin(); it != addedVec.end(); it++) {
-        std::cout << user[*it];
+        // std::cout << user[*it];
+        std::cout << *it;
     }
 
 
